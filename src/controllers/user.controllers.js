2f9a2223ec692ff06bfcf51b23ef2c57cdbfe2fb
MuhammadUsman7l <usmanjaleel223@gmail.com>
2024-08-08 14:31:19 +0500

    Login & Logout Conytollers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,25 @@ import { User } from "../models/user.models.js";
 import { uploadCLoudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiresponse.js";
 
+const generateAccessandRefreshToken = async (user_ID) => {
+  try {
+    const user = await User.findById(user_ID);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access and refresh token"
+    );
+  }
+};
+
+// Sign Up
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, fullname, password } = req.body;
   if (
@@ -24,7 +43,14 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImageLocalPath[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImageLocalPath) &&
+    req.files.coverImageLocalPath.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImageLocalPath[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar File is required");
@@ -59,4 +85,77 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, craetedUser, "User Successfully registered"));
 });
 
-export { registerUser };
+//login
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "Username or Email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "Invalid Username or Email");
+  }
+
+  const passwordValid = await user.isPasswordCorrect(password);
+
+  if (!passwordValid) {
+    throw new ApiError(401, "Invalid User Credential");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+    user._id
+  );
+  const logedIN = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(500)
+    .cookie("accessToken : ", accessToken, options)
+    .cookie("refreshToken : ", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: logedIN,
+          accessToken,
+          refreshToken,
+        },
+        "User loggedIn Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    res.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie(accessToken, options)
+    .cookie(refreshToken, options)
+    .json(new ApiResponse(200, {}, "User LogOut Successfully"));
+});
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controllers.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
 router.route("/register").post(
   upload.fields([
     {
@@ -17,4 +24,7 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
